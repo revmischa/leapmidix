8fd94701969ad5fbda8a998332f5b13966d7ea48
Mischa S <revmischa@cpan.org>
2012-12-01 01:40:39 -0800

    Initial Commit

---
+++ LeapMIDIX/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  LeapMIDIX
+//
+//  Created by Mischa Spiegelmock on 12/1/12.
+//  Copyright (c) 2012 DBA int80. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
