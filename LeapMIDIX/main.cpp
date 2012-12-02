07d57a551f70820127991f1a68f54a86d00c25f6
Mischa S <revmischa@cpan.org>
2012-12-01 19:28:23 -0800

    remove old junk

--- LeapMIDIX/main.cpp
+++ LeapMIDIX/main.cpp
@@ -26,26 +26,3 @@ int main(int argc, const char * argv[]) {
     
     return 0;
 }
-
-void sendNote() {
-    OSStatus result;
-    const unsigned kMySize = 32;
-    Byte list_buffer[kMySize];
-    MIDIPacketList *packetList = (MIDIPacketList*)list_buffer;
-    
-    Byte noteOn[3] = { 0x90, 40, 64 };
-    Byte noteOff[3] = { 0x90, 40, 0 };
-    
-    MIDIPacket *curPacket = MIDIPacketListInit(packetList);
-    
-    Byte note = rand() % 40 + 25;
-    noteOn[1] = note;
-    noteOff[1] = note;
-    curPacket = MIDIPacketListAdd(packetList, kMySize, curPacket, 0, 3, noteOn);
-    curPacket = MIDIPacketListAdd(packetList, kMySize, curPacket, mach_absolute_time() + 1000, 3, noteOff);
-    if (! curPacket) {
-        std::cerr << "Failed to add MIDI packet" << std::endl;
-        return;
-    }
-//    device->send(packetList);
-}
