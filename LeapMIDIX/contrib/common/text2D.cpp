76026563ab189847a720fe8d7d5edad9816b1c20
Mischa S <revmischa@cpan.org>
2012-12-23 14:50:02 -0800

    killing warns

--- LeapMIDIX/contrib/common/text2D.cpp
+++ LeapMIDIX/contrib/common/text2D.cpp
@@ -37,7 +37,7 @@ void initText2D(const char * texturePath){
 
 void printText2D(const char * text, int x, int y, int size){
 
-	unsigned int length = (GLsizei)strlen(text);
+	size_t length = strlen(text);
 
 	// Fill buffers
 	std::vector<glm::vec2> vertices;
@@ -101,7 +101,7 @@ void printText2D(const char * text, int x, int y, int size){
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 	// Draw call
-	glDrawArrays(GL_TRIANGLES, 0, (GLsizei)vertices.size() );
+	glDrawArrays(GL_TRIANGLES, 0, (int)vertices.size());
 
 	glDisable(GL_BLEND);
 
